972a9de0f4c977989df8dd0fdd14780fd11d7cf5
Ben Lippincott <benji.lippincott@gmail.com>
2017-03-29 10:53:20 -0400

    added time class

---
+++ tests/TimeTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use Benlipp\SrtParser\Time;
+use PHPUnit\Framework\TestCase;
+
+class TimeTest extends TestCase
+{
+
+    /**
+     * Check for possible syntax errors
+     */
+    public function testAnySyntaxError()
+    {
+        $var = new Time();
+        $this->assertTrue(is_object($var));
+        unset($var);
+    }
+
+    /**
+     * @dataProvider timeProvider
+     */
+    public function testTimes($time, $expected)
+    {
+        $this->assertEquals(Time::get($time), $expected);
+    }
+
+    public function timeProvider()
+    {
+        return [
+            ['00:00:00,000', 0],
+            ['00:04:30,000', 270],
+            ['01:20:30,999', 4830.999],
+        ];
+    }
+}
