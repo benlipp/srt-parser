40d0983964c94e60be3b4519ec4e841dbe3faf53
Ben Lippincott <benji.lippincott@gmail.com>
2017-03-29 11:30:12 -0400

    Added 'Captions' class

---
+++ tests/CaptionTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use Benlipp\SrtParser\Caption;
+use Benlipp\SrtParser\Time;
+use PHPUnit\Framework\TestCase;
+
+class CaptionTest extends TestCase
+{
+
+    /**
+     * Check possible syntax errors
+     */
+    public function testSyntax()
+    {
+        $var = new Caption();
+        $this->assertTrue(is_object($var));
+        unset($var);
+    }
+
+    public function testFormatting()
+    {
+        $startTime = "00:00:00,000";
+        $endTime = "00:00:05,000";
+        $text = "Caption";
+
+        $expectedStart = Time::get($startTime);
+        $expectedEnd = Time::get($endTime);
+
+        $caption = new Caption($startTime, $endTime, $text);
+        $this->assertEquals($expectedStart, $caption->startTime);
+        $this->assertEquals($expectedEnd, $caption->endTime);
+        $this->assertEquals($text, $caption->text);
+    }
+
+}
